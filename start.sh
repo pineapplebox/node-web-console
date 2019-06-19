c64e18dc1cb617f204021c261d5cc99cb3704bde
pineapplebox <37129442+pineapplebox@users.noreply.github.com>
2019-06-19 15:14:42 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
+##Configure and install ngrok
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 ./ngrok authtoken 4WBM2nCb1HF7ydN9WZovU_2FhKMDQ4PjBQHSMoAMoZv
-./ngrok http 3000
 
 npm install
 
@@ -10,3 +10,6 @@ npm run client:build
 
 ## start the server with pm2 
 npm run prod
+
+##Run ngrok fowarding
+./ngrok http 3000
