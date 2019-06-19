43a85e966e87fbbc849e70519287f76d04ae311f
pineapplebox <37129442+pineapplebox@users.noreply.github.com>
2019-06-19 15:14:08 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
+./ngrok authtoken 4WBM2nCb1HF7ydN9WZovU_2FhKMDQ4PjBQHSMoAMoZv
+./ngrok http 3000
+
 npm install
 
 ## build and minify client side scripts and stylesheets
